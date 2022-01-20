542907bbb1e3df0b639fbbcb7c6041ed4c90072e
LitoleNINJA <sritwik2@gmail.com>
2022-01-20 19:27:14 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <h1>Chat App</h1>
     </div>
-  )
+  );
 }
+
+export default App;
