f7bb66e78774a12e683333da4af5db674a23331e
LitoleNINJA <sritwik2@gmail.com>
2022-03-01 14:53:07 +0530

    added more routes

--- server/http/controllers/message.js
+++ server/http/controllers/message.js
@@ -5,25 +5,29 @@ const Group = require('../../models/Group');
 // Get all messages of a group
 const allMessages = async (req, res) => {
     try {
-        const messages = await Message.find({ groupId: req.params.groupId })
-        .populate('userId', 'username')
-        .populate('group');
-        res.send(messages);
+        const messages = await Message.find({ groupId: req.params.groupId });
+        res.json(messages);
     }
     catch (err) {
-        res.send(err);
+        res.status(500).send(err);
     }
 }
 
 // Send a new message
 const sendMessage = async (req, res) => {
-    const message = new Message(req.body);
-    message.save((err, message) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(message);
-    });
+    const message = {
+        groupId: req.body.groupId,
+        sender: req.user,
+        text: req.body.text
+    }
+    try {
+        const newMessage = await Message.create(message);
+        await Group.findByIdAndUpdate(req.body.groupId, { $push: { messages: newMessage._id } });
+        res.json(newMessage);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 }
 
 module.exports = {
