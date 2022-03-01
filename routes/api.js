94d1234230ef94e61bcc5ac82d2c85cfa1b9fdc2
LitoleNINJA <sritwik2@gmail.com>
2022-03-01 14:46:40 +0530

    modified routes

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,21 @@
 const Auth = require('../server/http/controllers/auth');
 const Group = require('../server/http/controllers/group');
 const Message = require('../server/http/controllers/message');
-const User = require('../server/http/middlewares/user');
+const user = require('../server/http/middlewares/user');
 
 const api = (app) => {
-    app.get('/api/auth/register', Auth.register);
-    app.get('/api/auth/login', Auth.login);
+    app.post('/api/auth/register', Auth.register);
+    app.post('/api/auth/login', Auth.login);
     app.get('/api/auth/verify/:token', Auth.verify);
 
-    app.post('/api/messages/', User, Message.sendMessage);
-    app.get('/api/messages/:groupId', User, Message.allMessages);
-    app.get('/api/group/:userId', User, Group.allGroups);
-    app.post('/api/group/', User, Group.createGroup);
+    app.post('/api/message', user, Message.sendMessage);
+    app.get('/api/message/:groupId', user, Message.allMessages);
+
+    app.get('/api/group', user, Group.allGroups);
+    app.post('/api/group', user, Group.createGroup);
+    app.get('/api/group/:groupId', user, Group.accessGroup);
+    app.post('/api/group/:groupId', user, Group.addToGroup);
+    app.put('/api/group/:groupId', user, Group.renameGroup);
 }
 
 module.exports = api;
