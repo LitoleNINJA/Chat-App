5052b4ad8ef2779ed3f2c3ecc78c45d075f0d919
Ritwik <sritwik03@gmail.com>
2022-05-14 09:48:24 +0530

    initial commit

---
+++ client/src/context/ChatProvider.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+    const [selectedChat, setSelectedChat] = useState();
+    const [chats, setChats] = useState();
+    const [notif, setnotif] = useState([]);
+    const [user, setUser] = useState(null);
+
+    const history = useHistory();
+
+    useEffect(() => {
+        const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        setUser(userInfo);
+        if(!userInfo) {
+            history.push('/');
+        }
+        else
+            history.push('/chat');
+    }, [history]);
+
+    return (
+        <ChatContext.Provider value={{
+            selectedChat,
+            setSelectedChat,
+            chats,
+            setChats,
+            notif,
+            setnotif,
+            user,
+            setUser,
+        }}>
+            {children}
+        </ChatContext.Provider>
+    );
+};
+
+export const ChatState = () => useContext(ChatContext);
+
+export default ChatProvider;
