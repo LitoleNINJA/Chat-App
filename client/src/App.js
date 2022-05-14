039ed6ae171a38d5777a33eaf746c45452102679
Ritwik <sritwik03@gmail.com>
2022-05-14 09:46:07 +0530

    initialize frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import axios from 'axios';
 
-export default function App() {
+import './App.css';
+import LandingPage from './pages/LandingPage';
+import ChatPage from './pages/ChatPage';
+import ChatProvider from './context/ChatProvider';
+
+function App() {
+  axios.defaults.baseURL = 'http://localhost:5000/api';
+  const theme = createTheme({
+    typography: {
+      allVariants: {
+        fontFamily: 'system-ui',
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <h1>Chat App</h1>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <Router>
+        <ChatProvider>
+          <Switch>
+            <Route exact path="/">
+              <LandingPage />
+            </Route>
+            <Route path="/chat">
+              <ChatPage />
+            </Route>
+          </Switch>
+        </ChatProvider>
+      </Router>
+    </ThemeProvider>
+  );
 }
+
+export default App;
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('./server/config/db')();
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+require('./routes/api')(app);
+
+app.use((err, req, res, next) => {
+    res.send({
+        error: err.message
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('setup', (userData) => {
+        console.log(userData._id);
+        socket.join(userData._id);
+        socket.emit('connected');
+    });
+
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log(`User joined room ${room}`);
+    });
+
+    socket.on('typing', (room) => {
+        socket.in(room).emit('typing');
+    });
+
+    socket.on('not typing', (room) => {
+        socket.in(room).emit('not typing');
+    });
+
+    socket.on('send message', (message, group) => {
+        group.members.forEach(member => {
+            if(member !== message.sender.userId) {
+                console.log('sending to', member);
+                socket.in(member).emit('message recieved', message);
+        }});
+    });
+
+    socket.on('disconnect', (userData) => {
+        console.log('User disconnected');
+        socket.leave(userData._id);
+    });
+});
