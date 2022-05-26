6ecb2b0b001c06e6346748c9d41045c4cc3e3d8d
Ritwik <sritwik03@gmail.com>
2022-05-26 14:19:36 +0530

    bugs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname1, '/client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname1, '/client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'));
     });
 }
 // -------------------------------------------------
